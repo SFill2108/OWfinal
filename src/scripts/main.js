40a347789e2e958789c1ec1e170042e5af99d8f4
Fill <gcodepost@gmail.com>
2019-07-30 23:32:23 +0300

    First commit

---
+++ src/scripts/main.js
@@ -0,0 +1,3 @@
+//=include jquery-3.4.1.min.js
+//=include ../../node_modules/slick-carousel/slick/slick.js
+//=include offerSlide.js
---
+++ src/scripts/offerSlide.js
@@ -0,0 +1,3 @@
+$('.offerSlide').slick({
+    arrows: false,
+});
